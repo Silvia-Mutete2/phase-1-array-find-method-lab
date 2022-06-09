d38284a4ed82839e98970e8e371d0d7017cd2fcc
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-09 15:21:46 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+//returns a year the Denver Broncos won the superbowl
+//returns undefined if the record has no win objects
+
+function superbowlWin (record){
+    const result = record.find
+    (record => record.result==="W")
+    return !!result ? result.year : undefined
+
+    }
+    
+
+    
+
+ 
